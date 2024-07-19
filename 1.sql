b0cd18d9f0f984df5cc0ac8b4f69c3cf29d5e5c0
Crushtain <vova.muzylev@mail.ru>
2024-07-19 15:52:36 +0300

    1.3 fixed

--- 1.sql
+++ 1.sql
@@ -14,8 +14,11 @@ group by department_id) j on d.id = j.department_id and count_id >= 2
 
 --1.3
 
-select d.id
+select d.*, dn.name
 from public.departments d
-join (select department_id, count(id) count_id
-from public.dep_names
-group by department_id) j on d.id = j.department_id and count_id >= 2
+left join (
+    select department_id, MIN(id) as min_id
+    from public.dep_names
+    group by department_id
+) min_dep_names on d.id = min_dep_names.department_id
+left join public.dep_names dn on min_dep_names.department_id = dn.department_id and min_dep_names.min_id = dn.id;
